de7a85b144095fac2b23015b44b406d5946c6f8e
Nate Chandler <nathaniel.chandler@gmail.com>
2014-05-15 22:56:30 -0400

    Generated all variations of NSView class.

---
+++ File Templates/Additions/BNR Objective-C class.xctemplate/NSViewBoilerplateTestHeader/___FILEBASENAME___.m
@@ -0,0 +1,30 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+#import "___FILEBASENAME____TestInterface.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
